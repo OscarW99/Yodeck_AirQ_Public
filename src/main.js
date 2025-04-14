ee854defc5c325e2f267b8ddc7cb233943276a13
OscarW99 <oscar-wright@mail.com>
2025-04-14 18:03:29 +0100

    feat: Update CSS import in main.js to match gh-pages branch; adjust index.html for correct stylesheet inclusion

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// Import the CSS file so Vite can process it
-import './styles/main.css';
+// Import the CSS file with the name that matches what's in the gh-pages branch
+import './styles/style.css';
 
 // Additional JavaScript imports or code can go here
 console.log('Main script loaded');
